81fc36631c2c172f86c4db165ce513aa3d144c05
HarryJoo <31430993+HarryJoo@users.noreply.github.com>
2020-03-23 23:57:01 +0900

    Update TimeConversion.java

--- warmup/TimeConversion.java
+++ warmup/TimeConversion.java
@@ -25,19 +25,19 @@ public class TimeConversion {
      * Complete the timeConversion function below.
      */
     static String timeConversion(String s) {
-		int hours = Integer.parseInt(s.split(":")[0]);
-		int minutes = Integer.parseInt(s.split(":")[1]);
-		int seconds = Integer.parseInt(s.split(":")[2].replaceAll("[^0-9]", ""));	
-		String ampm = s.split(":")[2].replaceAll("[^A-Z]", "");
-		
-		// AM/PM 시간을 24시간으로 변환
-		hours = hours % 12; 	// 1. hours를 12로 나눈 나머지 구하기
-		if("PM".equals(ampm)) hours += 12;	// 2. PM인 경우 hours에 12를 더하기
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append(String.format("%02d", hours)).append(":").append(String.format("%02d", minutes)).append(":").append(String.format("%02d", seconds));
-		
-		System.out.println(sb.toString());
+	int hours = Integer.parseInt(s.split(":")[0]);
+	int minutes = Integer.parseInt(s.split(":")[1]);
+	int seconds = Integer.parseInt(s.split(":")[2].replaceAll("[^0-9]", ""));	
+	String ampm = s.split(":")[2].replaceAll("[^A-Z]", "");
+
+	// AM/PM 시간을 24시간으로 변환
+	hours = hours % 12; 	// 1. hours를 12로 나눈 나머지 구하기
+	if("PM".equals(ampm)) hours += 12;	// 2. PM인 경우 hours에 12를 더하기
+
+	StringBuilder sb = new StringBuilder();
+	sb.append(String.format("%02d", hours)).append(":").append(String.format("%02d", minutes)).append(":").append(String.format("%02d", seconds));
+
+	System.out.println(sb.toString());
     	return sb.toString();
     }
 
